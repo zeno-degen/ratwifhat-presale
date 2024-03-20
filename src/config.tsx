7f68e75e76fa9fc1195bdfc3e14a2ee8e40bea6d
zeno-degen <tardis.abs@gmail.com>
2024-03-20 16:42:54 +0900

    feat: update contract and fix FE

--- src/config.tsx
+++ src/config.tsx
@@ -15,8 +15,9 @@ export const DEFAULT_GAS = BigInt("30000000");
 export const DEFAULT_GAS_PRICE = BigInt("2000000000000");
 export const BUY_AVAIABLE_PERCENT = 50;
 export const TOTAL_ETH_AMOUNT = 30;
+export const PRESALE_ENDED_TIME = 1711522800;
 export const TOKEN_AIRDROP_CONTRACT_ADDR =
-  "0xfCB4643a8f51c937175166E4e85Df7Add77Db115";
+  "0x3438aEC11c226B4f8B163A379d6A60b0827Edba0";
 
 export const SITE_LINK = "https://ratwifhat.xyz/";
 export const TWITTER_LINK = "https://twitter.com/Ratwifcoin";
